5cfddb66143fdeb0e3e3a4373604f6cc85b4b395
Varun H <151680240+HemanthVarun@users.noreply.github.com>
2024-06-30 19:14:36 +0530

    Update main.tf

--- EKS-Cluster/main.tf
+++ EKS-Cluster/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
